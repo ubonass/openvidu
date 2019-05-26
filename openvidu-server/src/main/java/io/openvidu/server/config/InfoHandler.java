edafd7f81b514b3741b2515bd2c3e182d52d9cbf
Jeffrey <you@example.com>
2019-05-26 17:36:33 +0800

    support userId in InfoHandler

--- openvidu-server/src/main/java/io/openvidu/server/config/InfoHandler.java
+++ openvidu-server/src/main/java/io/openvidu/server/config/InfoHandler.java
@@ -22,11 +22,10 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Semaphore;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 import io.openvidu.client.internal.ProtocolElements;
 import io.openvidu.server.rpc.RpcConnection;
+import org.kurento.client.IceCandidate;
 import org.kurento.jsonrpc.message.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,67 +35,112 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class InfoHandler extends TextWebSocketHandler {
-	
-	private static final Logger log = LoggerFactory.getLogger(InfoHandler.class);
-	
-	Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
-	Semaphore semaphore = new Semaphore(1);
-	
-	public void sendInfo(String info){
-		for (WebSocketSession session : this.sessions.values()) {
-			try {
-				this.semaphore.acquire();
-				session.sendMessage(new TextMessage(info));
-				this.semaphore.release();
-			} catch (IOException | InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	/**
-	 * modify by jeffrey for userId support
-	 * userId is client id 8bit
-	 * 在握手的时候设置了Attributes属性
-	 * @param session
-	 * @throws Exception
-	 */
-	@Override
-	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		log.info("Info websocket stablished...");
-		Map<String, Object> attributes = session.getAttributes();
-		log.info("user info:" + attributes);
-		for (String key : attributes.keySet()) {
-			log.info("key:" + key + " and value:" + attributes.get(key));
-		}
-		//this.sessions.put(session.getId(), session);
-		this.sessions.put(attributes.get("userId").toString(), session);
-	}
 
-	/**
-	 * modify by jeffrey
-	 * @param session
-	 * @param close
-	 * @throws Exception
-	 */
-	@Override
-	public void afterConnectionClosed(WebSocketSession session, CloseStatus close) throws Exception {
-		log.info("Info websocket closed: " + close.getReason());
-		//this.sessions.remove(session.getId());
-		this.sessions.remove(session.getAttributes().get("userId"));
-		session.close();
-	}
-	
-	@Override
-	protected void handleTextMessage(WebSocketSession session, TextMessage message)
-			throws Exception {
-		log.info("Message received: " + message.getPayload());
-	}
+    private static final Logger log = LoggerFactory.getLogger(InfoHandler.class);
 
-	/**
-	 * 例子为邀请4个人通话
-	 */
-	/*private void invited(RpcConnection rpcConnection, Request<JsonObject> request) {
-        *//*{
+    private static final Gson gson = new GsonBuilder().create();
+
+    Map<String, WebSocketSession> sessions = new ConcurrentHashMap<>();
+    Semaphore semaphore = new Semaphore(1);
+
+    public void sendInfo(String info) {
+        for (WebSocketSession session : this.sessions.values()) {
+            try {
+                this.semaphore.acquire();
+                session.sendMessage(new TextMessage(info));
+                this.semaphore.release();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 自定义函数
+     * 发送消息给指定的在线用户
+     */
+    public void sendMessageToUser(String userId, TextMessage message) {
+        try {
+            if (sessions.containsKey(userId)) {
+                WebSocketSession session = sessions.get(userId);
+                session.sendMessage(message);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error(e.getLocalizedMessage());
+        }
+    }
+
+    /**
+     * modify by jeffrey for userId support
+     * userId is client id 8bit
+     * 在握手的时候设置了Attributes属性
+     *
+     * @param session
+     * @throws Exception
+     */
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        log.info("Info websocket stablished...");
+        Map<String, Object> attributes = session.getAttributes();
+        log.info("user info:" + attributes);
+        for (String key : attributes.keySet()) {
+            log.info("key:" + key + " and value:" + attributes.get(key));
+        }
+        //this.sessions.put(session.getId(), session);
+        this.sessions.put(attributes.get("userId").toString(), session);
+    }
+
+    /**
+     * modify by jeffrey
+     *
+     * @param session
+     * @param close
+     * @throws Exception
+     */
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus close) throws Exception {
+        log.info("Info websocket closed: " + close.getReason());
+        //this.sessions.remove(session.getId());
+        this.sessions.remove(session.getAttributes().get("userId"));
+        session.close();
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message)
+            throws Exception {
+        JsonObject jsonMessage = gson.fromJson(message.getPayload(), JsonObject.class);
+        log.info("Message received: " + jsonMessage);
+        switch (jsonMessage.get("method").getAsString()) {
+            case ProtocolElements.INVITED_METHOD:
+                try {
+                    invited(session, jsonMessage);
+                } catch (Throwable t) {
+                    handleErrorResponse(t, session, ProtocolElements.INVITED_METHOD);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void handleErrorResponse(Throwable throwable, WebSocketSession session, String responseId)
+            throws IOException {
+        log.error(throwable.getMessage(), throwable);
+        JsonObject response = new JsonObject();
+        response.addProperty("method", responseId);
+        response.addProperty("response", "rejected");
+        response.addProperty("message", throwable.getMessage());
+        session.sendMessage(new TextMessage(response.toString()));
+    }
+
+
+
+    /**
+     * 例子为邀请4个人通话
+     */
+    private void invited(WebSocketSession session, JsonObject message) {
+        /*{
             "id":1,
             "method":"invited",
             "params":{
@@ -104,30 +148,37 @@ public class InfoHandler extends TextWebSocketHandler {
                     "session": "AAA",
                     "type": “all”,
                     "number": 4,
-                    "targets":[{"target_0":"dadasd","arget_1":"dadasd","arget_2":"dadasd","arget_3":"dadasd"}]
+                    "targets":[{"target_0":"dadasd","target_1":"dadasd","2":"target_2","target_3":"dadasd"}]
 
             },
             "jsonrpc":"2.0"
-        }
-        *//*
-		String userId = getStringParam(request, ProtocolElements.INVITED_USER_PARAM);
-		String sessionId = getStringParam(request, ProtocolElements.INVITED_ROOM_PARAM);
-		int number = getIntParam(request, ProtocolElements.INVITED_NUMBER_PARAM);
-		String targets = getStringParam(request, ProtocolElements.INVITED_TARGETS_PARAM);
-		String mediaType = getStringParam(request, ProtocolElements.INVITED_MEDIA_TYPE_PARAM);
-		*//**
-		 * 首先判断这个target id是否在userIdAndPrivateId集合当中有
-		 * 如果没有说明不在线需要返回,如果有则向目标发起通知,通知其加入房间
-		 *//*
-		if (number > 0) {
-			JsonArray targetArray =
-					new JsonParser().parse(targets).getAsJsonArray();
-			for (int i= 0;i<targetArray.size();i++) {
-				JsonObject object = targetArray.get(i).getAsJsonObject();
-				String targetId =
-						object.get("target_" + i).getAsString();
-				log.info("targetId : {}",targetId);
-			}
-		}
-	}*/
+        }*/
+
+        String params = message.get("params").getAsString();
+        JsonObject object = gson.fromJson(params, JsonObject.class);
+        log.info("object ==" + object.toString());
+
+        /*String userId = getStringParam(request, ProtocolElements.INVITED_USER_PARAM);
+        String sessionId = getStringParam(request, ProtocolElements.INVITED_ROOM_PARAM);
+        int number = getIntParam(request, ProtocolElements.INVITED_NUMBER_PARAM);
+        String targets = getStringParam(request, ProtocolElements.INVITED_TARGETS_PARAM);
+        String mediaType = getStringParam(request, ProtocolElements.INVITED_MEDIA_TYPE_PARAM);
+        *//**
+         * 首先判断这个target id是否在userIdAndPrivateId集合当中有
+         * 如果没有说明不在线需要返回,如果有则向目标发起通知,通知其加入房间*//*
+        if (number > 0) {
+            JsonArray targetArray =
+                    new JsonParser().parse(targets).getAsJsonArray();
+            for (int i = 0; i < targetArray.size(); i++) {
+                JsonObject object = targetArray.get(i).getAsJsonObject();
+                String targetId =
+                        object.get("target_" + i).getAsString();
+                log.info("targetId : {}", targetId);
+            }
+        }*/
+
+        JsonObject response = new JsonObject();
+        response.addProperty("method", ProtocolElements.INVITED_METHOD);
+        response.addProperty("response", "success");
+    }
 }
